570639aed2f751c5a8a26c7ce807928b2f996cf2
Dimas Febriyanto <dimassfeb@gmail.com>
2025-07-19 00:24:23 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: [
+                'resources/js/app.jsx', // <- JS utama kamu
+                'resources/css/app.css', // <- CSS utama kamu
+            ],
             refresh: true,
         }),
+        react(),
     ],
 });
