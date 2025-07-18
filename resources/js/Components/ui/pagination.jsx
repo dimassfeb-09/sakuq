db3a5d9625548fcfebfece55830ea1d2d7ed309a
Dimas Febriyanto <dimassfeb@gmail.com>
2025-07-18 20:58:53 +0700

    fix button

--- resources/js/Components/ui/pagination.jsx
+++ resources/js/Components/ui/pagination.jsx
@@ -1,7 +1,7 @@
 import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react';
 import * as React from 'react';
 
-import { Button } from '@/Components/ui/button';
+import { buttonVariants } from '@/Components/ui/button';
 import { cn } from '@/lib/utils';
 
 const Pagination = ({ className, ...props }) => (
